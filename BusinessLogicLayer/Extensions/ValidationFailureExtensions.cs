95257ea1c0c3c8c88c828858ee48d358dc3ea81e
JesusErazo <erazojesusmateo@gmail.com>
2025-07-08 16:32:42 -0500

    I added in orders service adition and updation implementations

---
+++ BusinessLogicLayer/Extensions/ValidationFailureExtensions.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.OrdersService.BusinessLogicLayer.DTO;
+using FluentValidation.Results;
+
+namespace eCommerce.OrdersService.BusinessLogicLayer.Extensions
+{
+  public static class ValidationFailureExtensions
+  {
+    public static ValidationFailureResponse ToBasicFormat(this IEnumerable<ValidationFailure> failures)
+    {
+      List<ValidationFailureItem> errors = failures.Select(error => new ValidationFailureItem(error.PropertyName, error.ErrorMessage)).ToList();
+      return new ValidationFailureResponse(errors);
+    }
+  }
+}
