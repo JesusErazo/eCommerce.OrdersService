54f81843e2d6215775a940fa0fe596716e29029e
JesusErazo <erazojesusmateo@gmail.com>
2025-07-08 13:00:16 -0500

    I added order mappers

---
+++ BusinessLogicLayer/Mappers/OrderUpdateRequestToOrderMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using eCommerce.OrdersService.BusinessLogicLayer.DTO;
+using eCommerce.OrdersService.DataAccessLayer.Entities;
+
+namespace eCommerce.OrdersService.BusinessLogicLayer.Mappers;
+
+public class OrderUpdateRequestToOrderMappingProfile : Profile
+{
+  public OrderUpdateRequestToOrderMappingProfile()
+  {
+    CreateMap<OrderUpdateRequest, Order>()
+      .ForMember(order => order._id, opt => opt.Ignore())
+      .ForMember(order => order.TotalBill, opt => opt.Ignore());
+  }
+}
